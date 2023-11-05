7d9c7c092bdedd1b0d3d8486a622f98a99b61b32
danilosantanaa <38994152+danilosantanaa@users.noreply.github.com>
2023-11-04 21:34:22 -0300

    Correções de bugs: part 1

--- misExperimento.cpp
+++ misExperimento.cpp
@@ -1,133 +1,141 @@
 #include <iostream>
 #include <vector>
+#include <set>
 
 using namespace std;
 
 class Graph {
-    public:
-    vector<vector<string>> adjacency_matrix;
+	public:
+		vector<vector<string>> adjacency_matrix;
 
-    Graph(int tam)
-    {
-        vector<string> line;
-        line.assign(tam, "0");
-        this->adjacency_matrix.assign(tam, line);
-    }
+		Graph(int tam) {
+			vector<string> line;
+			line.assign(tam, "0");
+			this->adjacency_matrix.assign(tam, line);
+		}
 
-    void addVertex(int src, int dst, string value) 
-    {
-       this->adjacency_matrix[src-1][dst-1] = value;
-    }
+		void addVertex(int src, int dst, string value) {
+			this->adjacency_matrix[src-1][dst-1] = value;
+		}
 
+		/**
+		 * 
+		 * Removo o grafico que as posições está marcado para ser deletado
+		*/
+		void remove() {
+			for(int line = this->adjacency_matrix.size() - 1; line >= 0; line--) {
+				auto it = this->vertexDeletedSet.find(line);
 
-    void remove(int pos) 
-    {
-        this->adjacency_matrix.erase(this->adjacency_matrix.begin() + pos);
-    }
+				if(this->isVertexDeletedTrack(line))
+				{
+					this->adjacency_matrix.erase(this->adjacency_matrix.begin() + line);
+					cout << "Deleted " << endl;
+				}
+			}
 
-    void print()
-    {
-        cout <<  "=== MATRIZ ADJACENCIA ====" << endl;
-        for(int line = 0; line < this->adjacency_matrix.size(); line++) 
-        {
-            cout << "pos " << line + 1 << ": ";
-            for(auto column: this->adjacency_matrix[line]) 
-            {
-                cout << "\t" << column << " ";
-            }
-            cout << endl;
-        }
-    }
+			this->vertexDeletedSet.clear();
+		}
 
-    Graph copy()
-    {
-        Graph new_graph(this->adjacency_matrix.size());
+		void print() {
+			cout <<  "=== MATRIZ ADJACENCIA ====" << endl;
+			for(int line = 0; line < this->adjacency_matrix.size(); line++) {
+				cout << "pos " << line + 1 << ": ";
+				for(auto column: this->adjacency_matrix[line]) {
+					cout << "\t" << column << " ";
+				}
+				cout << endl;
+			}
+		}
 
-        for(int i = 0; i < this->adjacency_matrix.size(); i++) 
-        {
-            for(int j = 0; j < this->adjacency_matrix.size(); j++)  
-            {
-                new_graph.addVertex(i+1, j+1, this->adjacency_matrix[i][j]);
-            }
-        }
+		Graph copy() {
+			Graph new_graph(this->adjacency_matrix.size());
 
-        return new_graph;
-    }
+			for(int i = 0; i < this->adjacency_matrix.size(); i++) {
+				for(int j = 0; j < this->adjacency_matrix.size(); j++) {
+					new_graph.addVertex(i+1, j+1, this->adjacency_matrix[i][j]);
+				}
+			}
 
-    bool isEmpty(Graph* graph) 
-    {
-        bool is_empty = true;
+			return new_graph;
+		}
 
-        for(auto i: graph->adjacency_matrix)
-        {
-            for(auto j: i)
-            {
-                is_empty = is_empty && j == "0";
-            }
-        }
+		bool isEmpty(Graph* graph) {
+			bool is_empty = true;
 
-        return is_empty;
-    }
+			for(auto i: graph->adjacency_matrix) {
+				for(auto j: i) {
+					is_empty = is_empty && j == "0";
+				}
+			}
 
-    int min_pos(Graph* graph)
-    {
-        int pos_min_vertex;
-        int min_degree;
+			return is_empty;
+		}
 
-        for(int line = 0; line < graph->adjacency_matrix.size(); line++)
-        {
-            int tot_degree = 0;
-            for(auto column: graph->adjacency_matrix[line])
-            {
-                if(column == "1") tot_degree++;
-            }
+		int min_pos(Graph* graph) {
+			int pos_min_vertex;
+			int min_degree;
 
-            if(line == 0 || tot_degree < min_degree) 
-            {
-                min_degree = tot_degree;
-                pos_min_vertex = line;
-            }
-        }
+			for(int line = 0; line < graph->adjacency_matrix.size(); line++) {
+				int tot_degree = 0;
+				for(auto column: graph->adjacency_matrix[line]) {
+					if(column == "1") tot_degree++;
+				}
 
-        return pos_min_vertex;
-    }
+				if(line == 0 || tot_degree < min_degree) {
+					min_degree = tot_degree;
+					pos_min_vertex = line;
+				}
+			}
 
-    void neighbor(Graph* graph, int min_degree)
-    {
-        for(int i = 0; i < graph->adjacency_matrix.size(); i++)
-        {
-            if(graph->adjacency_matrix[min_degree][i] == "1")
-            {
-                graph->remove(i);
-            }
-        }
-        cout << min_degree << endl;
-        graph->print();
-        cout << "====" << endl;
-    }
+			return pos_min_vertex;
+		}
 
-    vector<int> mis()
-    {
-        vector<int> independent_set;
-        Graph graph = this->copy();
+		void addNeighborDelete(Graph* graph, int min_degree) {
+			for(int i = 0; i < graph->adjacency_matrix.size(); i++) {
+				if(graph->adjacency_matrix[min_degree][i] == "1") {
+					graph->addVertexDelete(i);
+				}
+			}
+		}
 
-        cout << "Teste" << endl;
-        cout << isEmpty(&graph) << endl;
-        graph.print();
+		void addVertexDegreeMinDelete(Graph* graph, int min_degree)
+		{
+			graph->addVertexDelete(min_degree);
+		}
 
-        while(!isEmpty(&graph)) 
-        {
-            int min_degree_vertex = min_pos(&graph);
+		vector<int> mis() {
+			vector<int> independent_set;
+			Graph graph = this->copy();
 
-            independent_set.push_back(min_degree_vertex);
+			while(!isEmpty(&graph)) {
+				int vertex_min_degree_position = min_pos(&graph);
 
-            this->neighbor(&graph, min_degree_vertex);
-            
-            this->remove(min_degree_vertex);
-        }
+				independent_set.push_back(vertex_min_degree_position);
 
-        return independent_set;
-    }
+				graph.addNeighborDelete(&graph, vertex_min_degree_position);
+				graph.addVertexDegreeMinDelete(&graph, vertex_min_degree_position);
+				graph.remove();
+			}
+
+			graph.print();
+
+			return independent_set;
+		}
+		
+		private:
+			set<int> vertexDeletedSet;
+			
+			void addVertexDelete(int vertex)
+			{
+				vertexDeletedSet.insert(vertex);
+			}
+
+			bool isVertexDeletedTrack(int vertex_search){
+				for(auto element: vertexDeletedSet) {
+					if(vertex_search == element) return true;
+				}
+				return false;
+			}
 
 };
 
@@ -135,63 +143,62 @@ class Graph {
 /** HEADER */
 void teste1(Graph* graph);
 
-int main()
-{
-    Graph graph(6);
+int main() {
+	Graph graph(6);
 
-    teste1(&graph);
+	teste1(&graph);
 
-    vector<int> i = graph.mis();
+	vector<int> i = graph.mis();
 
-    for(auto t: i) {
-        printf("%d\n", t);
-    }
+	for(auto t: i) {
+		printf("%d\n", t + 1);
+	}
 
-    graph.print();
+	graph.print();
 
-    return 0;
+	return 0;
 }
 
 void teste1(Graph* graph) {
-    graph->addVertex(1, 1, "0");
-    graph->addVertex(1, 2, "1");
-    graph->addVertex(1, 3, "1");
-    graph->addVertex(1, 4, "0");
-    graph->addVertex(1, 5, "1");
-    graph->addVertex(1, 6, "0");
+	graph->addVertex(1, 1, "0");
+	graph->addVertex(1, 2, "1");
+	graph->addVertex(1, 3, "1");
+	graph->addVertex(1, 4, "0");
+	graph->addVertex(1, 5, "1");
+	graph->addVertex(1, 6, "0");
 
-    graph->addVertex(2, 1, "1");
-    graph->addVertex(2, 2, "0");
-    graph->addVertex(2, 3, "0");
-    graph->addVertex(2, 4, "0");
-    graph->addVertex(2, 5, "1");
-    graph->addVertex(2, 6, "0");
+	graph->addVertex(2, 1, "1");
+	graph->addVertex(2, 2, "0");
+	graph->addVertex(2, 3, "0");
+	graph->addVertex(2, 4, "0");
+	graph->addVertex(2, 5, "1");
+	graph->addVertex(2, 6, "0");
 
-    graph->addVertex(3, 1, "1");
-    graph->addVertex(3, 2, "0");
-    graph->addVertex(3, 3, "0");
-    graph->addVertex(3, 4, "0");
-    graph->addVertex(3, 5, "1");
-    graph->addVertex(3, 6, "1");
+	graph->addVertex(3, 1, "1");
+	graph->addVertex(3, 2, "0");
+	graph->addVertex(3, 3, "0");
+	graph->addVertex(3, 4, "0");
+	graph->addVertex(3, 5, "1");
+	graph->addVertex(3, 6, "1");
 
-    graph->addVertex(4, 1, "0");
-    graph->addVertex(4, 2, "0");
-    graph->addVertex(4, 3, "0");
-    graph->addVertex(4, 4, "0");
-    graph->addVertex(4, 5, "0");
-    graph->addVertex(4, 6, "1");
+	graph->addVertex(4, 1, "0");
+	graph->addVertex(4, 2, "0");
+	graph->addVertex(4, 3, "0");
+	graph->addVertex(4, 4, "0");
+	graph->addVertex(4, 5, "0");
+	graph->addVertex(4, 6, "1");
 
-    graph->addVertex(5, 1, "1");
-    graph->addVertex(5, 2, "1");
-    graph->addVertex(5, 3, "1");
-    graph->addVertex(5, 4, "0");
-    graph->addVertex(5, 5, "0");
-    graph->addVertex(5, 6, "1");
+	graph->addVertex(5, 1, "1");
+	graph->addVertex(5, 2, "1");
+	graph->addVertex(5, 3, "1");
+	graph->addVertex(5, 4, "0");
+	graph->addVertex(5, 5, "0");
+	graph->addVertex(5, 6, "1");
 
-    graph->addVertex(6, 1, "0");
-    graph->addVertex(6, 2, "0");
-    graph->addVertex(6, 3, "0");
-    graph->addVertex(6, 4, "1");
-    graph->addVertex(6, 5, "1");
-    graph->addVertex(6, 6, "0");
+	graph->addVertex(6, 1, "0");
+	graph->addVertex(6, 2, "0");
+	graph->addVertex(6, 3, "1");
+	graph->addVertex(6, 4, "1");
+	graph->addVertex(6, 5, "1");
+	graph->addVertex(6, 6, "0");
 }
