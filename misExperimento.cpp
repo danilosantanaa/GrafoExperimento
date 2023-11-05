43b6bdc09b62564c4fae766b2bde0d11f24f8fe0
danilosantanaa <38994152+danilosantanaa@users.noreply.github.com>
2023-11-05 00:45:24 -0300

    Refataração simples

--- misExperimento.cpp
+++ misExperimento.cpp
@@ -17,7 +17,7 @@ class Graph {
 		}
 
 		void addVertex(int src, int dst, string value) {
-			this->adjacency_matrix[src-1][dst-1] = value;
+			this->adjacency_matrix[src][dst] = value;
 		}
 
 		void print() {
@@ -38,7 +38,7 @@ class Graph {
 
 			for(int i = 0; i < this->adjacency_matrix.size(); i++) {
 				for(int j = 0; j < this->adjacency_matrix.size(); j++) {
-					new_graph.addVertex(i+1, j+1, this->adjacency_matrix[i][j]);
+					new_graph.addVertex(i, j, this->adjacency_matrix[i][j]);
 				}
 			}
 
@@ -47,28 +47,30 @@ class Graph {
 
 		bool isEmpty() {
 			bool is_empty = true;
+
 			for(int line = 0; line < this->adjacency_matrix.size(); line++) {
 				is_empty = is_empty && this->isVertexIguinor(line);
 			}
+
 			return is_empty;
 		}
 
-		int min_pos() {
+		int minimusVertexPosition() {
 			int pos_min_vertex;
-			int min_degree;
+			int minimus_degree;
 
 			for(int line = 0; line < this->adjacency_matrix.size(); line++) {
 
 				if(this->isVertexIguinor(line)) continue;
-				
+
 				int tot_degree = 0;
 				for(auto column: this->adjacency_matrix[line]) {
 					if(column == "1") tot_degree++;
 				}
-				
-				bool is_min = line == 0 || tot_degree < min_degree;
+
+				bool is_min = line == 0 || tot_degree < minimus_degree;
 				if(is_min) {
-					min_degree = tot_degree;
+					minimus_degree = tot_degree;
 					pos_min_vertex = line;
 				}
 			}
@@ -76,148 +78,167 @@ class Graph {
 			return pos_min_vertex;
 		}
 
-		void addNeighborDelete(int min_degree) {
-			for(int i = 0; i < this->adjacency_matrix.size(); i++) {
-				
-				if(this->adjacency_matrix[min_degree][i] == "1" && i != min_degree) {
-					this->addVertexDelete(i);
+		void addNeighborStatusDeleted(int minimus_vertex_degree_position) {
+			for(int vertexNeighborPos = 0; vertexNeighborPos < this->adjacency_matrix.size(); vertexNeighborPos++) {
+
+				if(
+					this->adjacency_matrix[minimus_vertex_degree_position][vertexNeighborPos] == "1" && 
+					vertexNeighborPos != minimus_vertex_degree_position) 
+				{
+					this->addVertexDeleted(vertexNeighborPos);
 				}
 			}
 		}
 
-		void addVertexDegreeMinDelete(int min_degree)
-		{
-			this->addVertexDelete(min_degree);
+		void addVertexDegreeMinStatusDeleted(int minimus_vertex_degree_position) {
+			this->addVertexDeleted(minimus_vertex_degree_position);
 		}
 
 		Graph mis() {
 			Graph graph = this->copy();
 			while(!graph.isEmpty()) {
-				int vertex_min_degree_position = graph.min_pos();
+				int vertex_min_degree_position = graph.minimusVertexPosition();
 				graph.independent_set.push_back(vertex_min_degree_position);
-				graph.addNeighborDelete(vertex_min_degree_position);
-				graph.addVertexDegreeMinDelete(vertex_min_degree_position);
+				graph.addNeighborStatusDeleted(vertex_min_degree_position);
+				graph.addVertexDegreeMinStatusDeleted(vertex_min_degree_position);
 			}
 			return graph;
 		}
 
-		void showResult() {
+		void showResult(bool is_began_with_zero_vertex = false) {
 			cout << "CONJUNTO MAXIMAL: [" << this->independent_set.size() << "]" << endl;
 			cout << "{ ";
-			for(auto i: this->independent_set) {
-				cout << i << " ";
-			}
-			cout << "}" << endl << endl;
-		}
 		
-		private:
-			set<int> vertexDeletedSet;
-			
-			void addVertexDelete(int vertex)
-			{
-				vertexDeletedSet.insert(vertex);
-			}
+			for(int pos = 0; this->independent_set.size(); pos++) {
+				auto vertex = this->independent_set[pos];
 
-			bool isVertexIguinor(int vertex_search){
-				for(auto element: vertexDeletedSet) {
-					if(vertex_search == element) return true;
-				}
-				return false;
+				cout << ( is_began_with_zero_vertex ? vertex : vertex + 1 );
+				cout << ( pos < this->independent_set.size() ? ", " : "" );
 			}
+			cout << " }" << endl << endl;
+		}
+
+	private:
+		set<int> vertexDeletedSet;
+
+		void addVertexDeleted(int vertex) {
+			vertexDeletedSet.insert(vertex);
+		}
+
+		bool isVertexIguinor(int vertex_search) {
+			for(auto element: vertexDeletedSet) {
+				if(vertex_search == element) return true;
+			}
+			return false;
+		}
 };
 
 class FileHandler {
 	public:
-	Graph graph;
+		Graph graph;
 
-	FileHandler(Graph graph) {
-		this->graph = graph;
-	}
-
-	int getColumnTotal(char* buffer) 
-	{
-		const char delimiter[2] = " ";
-		char* token;
-		char newBuffer[TAM_BUFFER_READ];
-		
-		strcpy(newBuffer, buffer);
-		
-		/** Obter o primeiro token */
-		token = strtok(newBuffer, delimiter);
-		
-		/** Obter o segundo token */
-		int pos = 0;
-		while(token != NULL) 
-		{
-			token = strtok(NULL, delimiter);
-			pos++;
+		FileHandler(Graph graph) {
+			this->graph = graph;
 		}
-		
-		return pos;
-	}
 
-	void tokenize(char* buffer, int pos_line)
-	{
-		const char delimter[2] = " ";
-		char* token;
-		
-		/** Obter o primeiro token */
-		token = strtok(buffer, delimter);
-		
-		/** Obter os restantes dos tokens */
-		int pos_column = 0;
-		while(token != NULL) 
-		{
-			int last_posicional = strlen(token) - 1;
-			bool is_new_break_scape = token[last_posicional] == '\n';
-			if(is_new_break_scape) 
-			{
-				token[last_posicional] = '\0';
+		int getColumnTotal(char* buffer) {
+			const char delimiter[2] = " ";
+			char* token;
+			char newBuffer[TAM_BUFFER_READ];
+
+			strcpy(newBuffer, buffer);
+
+			/** Obter o primeiro token */
+			token = strtok(newBuffer, delimiter);
+
+			/** Obter o segundo token */
+			int pos = 0;
+			while(token != NULL) {
+				token = strtok(NULL, delimiter);
+				pos++;
 			}
 
-			this->graph.addVertex(pos_line+1, pos_column+1, token);
-			pos_column++;
-			token = strtok(NULL, delimter);
-		}
-	}
-
-	Graph read(string file_source) 
-	{
-		FILE* fptr = fopen(file_source.c_str(), "r");
-		
-		if(fptr == NULL) {
-			cout << "<< ERROR! Arquivo nao encontrado! >> " << endl;
-			exit(100);
+			return pos;
 		}
 
-		char buffer[TAM_BUFFER_READ];
-		int totalLine = 0;
-		while(fgets(buffer, TAM_BUFFER_READ, fptr)) 
-		{
-			bool is_first_buffer_catch = totalLine == 0;
-			if(is_first_buffer_catch) 
-			{
-				int totalColumn = this->getColumnTotal(buffer);
-				cout << totalColumn << endl;
-				this->graph.createAdjacenteMatrix(totalColumn);
+		void tokenize(char* buffer, int pos_line) {
+			const char delimter[2] = " ";
+			char* token;
+			char newBuffer[TAM_BUFFER_READ];
+
+			strcpy(newBuffer, buffer);
+
+			/** Obter o primeiro token */
+			token = strtok(newBuffer, delimter);
+
+			/** Obter os restantes dos tokens */
+			int pos_column = 0;
+			while(token != NULL) {
+				int last_posicional = strlen(token) - 1;
+				bool is_new_break_scape = token[last_posicional] == '\n';
+				if(is_new_break_scape) {
+					token[last_posicional] = '\0';
+				}
+
+				this->graph.addVertex(pos_line, pos_column, token);
+				pos_column++;
+				token = strtok(NULL, delimter);
+			}
+		}
+
+		Graph read(string file_source) {
+			FILE* fptr = fopen(file_source.c_str(), "r");
+
+			if(fptr == NULL) {
+				cout << "<< ERROR! Arquivo nao encontrado! >> " << endl;
+				exit(100);
 			}
 
-			this->tokenize(buffer, totalLine);
+			char buffer[TAM_BUFFER_READ];
+			int totalLine = 0;
+			while(fgets(buffer, TAM_BUFFER_READ, fptr)) {
 
-			totalLine++;
+				bool is_first_buffer_catch = totalLine == 0;
+
+				if(is_first_buffer_catch) {
+					int totalColumn = this->getColumnTotal(buffer);
+					this->graph.createAdjacenteMatrix(totalColumn);
+				}
+				
+				this->tokenize(buffer, totalLine);
+
+				totalLine++;
+			}
+
+			return this->graph;
 		}
-
-		return this->graph;
-	}
 };
 
 int main() {
-	Graph graph;
-	FileHandler file(graph);
+	string input;
+	do {
+		cout << "Informe o camanho do arquivo [sair: termina de executar]: ";
+		cin >> input;
 
-	graph = file.read("testes/teste01.txt");
-	graph.print();
-	graph.mis().showResult();
+		if(strcmp(input.c_str(), "sair") == 0) break;
+
+		try {
+			Graph graph;
+			FileHandler file(graph);
+
+			graph = file.read(input);
+			graph.print();
+			graph.mis().showResult();
+		} catch(exception &e) {
+			cout << "Exception: " << e.what() << endl;
+		}
+		
+		system("pause");
+		system("cls");
+	} 
+	while(strcmp(input.c_str(), "sair") != 0);
+
+	cout << "Bye Bye!" << endl;
 
 	return 0;
 }
